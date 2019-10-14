ab9b0d2eb37e0611d90a6e79de2c7bcd7766dbad
Jay Shah <j24shah@gmail.com>
2019-10-13 20:43:49 -0400

    created app genre

--- catalog/settings.py
+++ catalog/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # local apps.
+    'genre.apps.GenreConfig',
+    
     # Third party.
     'rest_framework',
 ]
