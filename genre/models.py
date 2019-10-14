7f7581c7ded8bab2743b396966ee69e04aa82222
Jay Shah <j24shah@gmail.com>
2019-10-13 20:51:07 -0400

    Created Genre model

--- genre/models.py
+++ genre/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Genre(models.Model):
+    genre = models.fields.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.genre
+
+
