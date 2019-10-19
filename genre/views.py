4e064df0721c43a32b5f1f6bd91f7ec3be89bd99
Jay Shah <j24shah@gmail.com>
2019-10-19 16:06:00 -0400

    added in movies page.
    need to refactor movies page.

--- genre/views.py
+++ genre/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from .models import Genre
+from .models import Genre, Movie
 
 
 # Create your views here.
 def index(request):
     genres = Genre.objects.all()
     return render(request, 'genre/index.html', context={'genres':genres} )
+
+def show_movies(request, pk):
+    genre = Genre.objects.get(pk=pk)
+    movies = Movie.objects.filter(genre=genre)
+    return render(request, 'genre/movies.html', context={'movies':movies})
